1ae0fa893f896118026bb9514943d26d8fbce3ee
Appy <appyjack886@gmail.com>
2022-03-12 13:26:49 +0530

    splashscreen initial commit.

---
+++ app/src/main/java/com/example/kawach/splashscreen/Splashscreen4Activity.java
@@ -0,0 +1,16 @@
+package com.example.kawach.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kawach.R;
+
+public class Splashscreen4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen4);
+    }
+}
