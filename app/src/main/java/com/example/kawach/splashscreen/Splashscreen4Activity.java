6e025d18e15d7138226c36814357bf8cfd662bb5
Appy <appyjack886@gmail.com>
2022-03-12 14:08:54 +0530

    splashscreen commit. Made splashscreen activity file and started with lottie animation.

--- app/src/main/java/com/example/kawach/splashscreen/Splashscreen4Activity.java
+++ app/src/main/java/com/example/kawach/splashscreen/Splashscreen4Activity.java
@@ -2,15 +2,27 @@ package com.example.kawach.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.example.kawach.MainActivity;
 import com.example.kawach.R;
 
 public class Splashscreen4Activity extends AppCompatActivity {
+    private static int SPLASH_SCREEN_TIME_OUT=4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen4);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splashscreen4Activity.this, MainActivity.class));
+                finish();
+            }
+        },SPLASH_SCREEN_TIME_OUT);
+
     }
 }
